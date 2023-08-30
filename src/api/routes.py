3ead365143cf27cc0ae227e14edb52a9e34023c7
Luis Marin <80131357+LuisMarin-dev@users.noreply.github.com>
2023-08-30 20:27:04 +0000

    added salt to the '/register' routeand created the '/login' route with its hashing algorithm

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,8 +4,65 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from werkzeug.security import generate_password_hash, check_password_hash
 
 api = Blueprint('api', __name__)
+
+def set_password(password, salt):
+    return generate_password_hash(f"{password}{salt}")
+
+def check_password(hash_password, password):
+    return check_password_hash(hash_password, password)
+
+@api.route('/user', methods=['POST']) #####lm83023'
+def add_user():
+    if request.method == 'POST':
+        body = request.json
+        email = body.get("email", None)
+        password = body.get("password", None)
+
+        if email is None or password is None:
+            return jsonify("You need an email and a password!1"), 400
+
+        else:
+            salt = 1
+            password = set_password(password)
+            user = User(email=email, password=password, salt=salt)
+            db.session.add(user)
+            try:
+                db.session.commit()
+                return jsonify({"message":"User created"}), 201
+            except Exception as err:
+                print(err.args)
+                db.session.rollback()
+                return jsonify({"message":f"error: {err.args}"}), 500
+
+
+@api.route('/login', methods=['POST'])
+def handle_login():
+    if request.method == 'POST':
+        body = request.json
+        email = body.get("email", None)
+        password = body.get("password", None)
+
+        if email is None and password is None:
+            return jsonify("You need an email and a password."), 400
+        else:
+            user = User.query.filter_by(email=email).one_or_none()
+            if user is None:
+                return jsonify({"message":"Bad credentials"}), 400
+            else:
+                # if check_password(user.password, password):
+                if check_password(user.password, password):
+                    return jsonify({"tokendeejemplo": "37489236757928970"}), 200
+                else:
+                    return jsonify({"message":"Bad credentials"}), 400
+
+
+
+
+
+
 
 
 # @api.route('/register', methods=['POST'])
@@ -22,7 +79,3 @@ api = Blueprint('api', __name__)
 #     return jsonify(data), 200
 
 
-@api.route('/user', methods=['POST']) #####lm83023'
-def add_user():
-    if request.method == 'POST':
-        body = request.json
